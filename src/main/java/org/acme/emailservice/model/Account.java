81a0cefecd6150c10fe7aa364ffa1432c3c0b694
1token <zboran@federizer.com>
2020-11-27 18:59:55 +0100

    account imap/smtp

--- src/main/java/org/acme/emailservice/model/Account.java
+++ src/main/java/org/acme/emailservice/model/Account.java
@@ -2,7 +2,6 @@ package org.acme.emailservice.model;
 
 import java.util.Date;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import javax.json.bind.annotation.JsonbTransient;
@@ -33,13 +32,31 @@ public class Account {
     @ManyToOne()
     @JsonbTransient
     @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
-    private  User user;
+    private User user;
 
     @Column(unique = false, nullable = true)
     private String displayName;
 
     @Column(updatable = false, unique = true, nullable = false)
     private String username;
+
+    @Column(unique = false, nullable = true)
+    private String imapAddr;
+    @Column(unique = false, nullable = true)
+    private int imapPort;
+    @Column(unique = false, nullable = true)
+    private int imapType;
+    @Column(unique = false, nullable = true)
+    private String imapSecret;
+
+    @Column(unique = false, nullable = true)
+    private String smtpAddr;
+    @Column(unique = false, nullable = true)
+    private int smtpPort;
+    @Column(unique = false, nullable = true)
+    private int smtpType;
+    @Column(unique = false, nullable = true)
+    private String smtpSecret;
 
     @OneToMany(mappedBy = "account", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Set<Message> messages = new HashSet<Message>();
@@ -70,6 +87,70 @@ public class Account {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getImapAddr() {
+        return imapAddr;
+    }
+
+    public void setImapAddr(String imapAddr) {
+        this.imapAddr = imapAddr;
+    }
+
+    public int getImapPort() {
+        return imapPort;
+    }
+
+    public void setImapPort(int imapPort) {
+        this.imapPort = imapPort;
+    }
+
+    public int getImapType() {
+        return imapType;
+    }
+
+    public void setImapType(int imapType) {
+        this.imapType = imapType;
+    }
+
+    /* public String getImapSecret() {
+        return imapSecret;
+    } */
+
+    public void setImapSecret(String imapSecret) {
+        this.imapSecret = imapSecret;
+    }
+
+    public String getSmtpAddr() {
+        return smtpAddr;
+    }
+
+    public void setSmtpAddr(String smtpAddr) {
+        this.smtpAddr = smtpAddr;
+    }
+
+    public int getSmtpPort() {
+        return smtpPort;
+    }
+
+    public void setSmtpPort(int smtpPort) {
+        this.smtpPort = smtpPort;
+    }
+
+    public int getSmtpType() {
+        return smtpType;
+    }
+
+    public void setSmtpType(int smtpType) {
+        this.smtpType = smtpType;
+    }
+
+    /* public String getSmtpSecret() {
+        return smtpSecret;
+    } */
+
+    public void setSmtpSecret(String smtpSecret) {
+        this.smtpSecret = smtpSecret;
     }
 
     public Set<Message> getMessages() {
