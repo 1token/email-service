3bef8109280956df6a6bd476956f463b70c5afcd
1token <zboran@federizer.com>
2020-11-28 09:12:39 +0100

    labels color, parent/child

--- src/main/java/org/acme/emailservice/model/Label.java
+++ src/main/java/org/acme/emailservice/model/Label.java
@@ -1,12 +1,15 @@
 package org.acme.emailservice.model;
 
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import javax.json.bind.annotation.JsonbTransient;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -14,6 +17,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -30,6 +34,13 @@ public class Label {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "parent_id", referencedColumnName = "id", nullable = true)
+    private Label parent;
+
+    @OneToMany(mappedBy = "parent", orphanRemoval = true, fetch = FetchType.EAGER)
+    private Set<Label> child = new HashSet<>();
+
     @ManyToOne()
     @JsonbTransient
     @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
@@ -40,6 +51,8 @@ public class Label {
     private Set<Message> message;
 
     private String name;
+
+    private int color;
 
     @SequenceGenerator(name="labelHistoryId", sequenceName="label_history_id")
     @GeneratedValue(generator="labelHistoryId", strategy = GenerationType.SEQUENCE)
@@ -61,12 +74,36 @@ public class Label {
         this.id = id;
     }
 
+    public Set<Label> getChild() {
+        return child;
+    }
+
+    public void setChild(Set<Label> child) {
+        this.child = child;
+    }
+
+    public Label getParent() {
+        return parent;
+    }
+
+    public void setParent(Label parent) {
+        this.parent = parent;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
     }
 
     public Set<Message> getMessage() {
