2641426e057694bab4fbc904fe1c856aee1ca647
1token <zboran@federizer.com>
2020-12-23 16:16:12 +0100

    GmailClient/SynchronizedTask

--- src/main/java/org/acme/emailservice/email/EmailService.java
+++ src/main/java/org/acme/emailservice/email/EmailService.java
@@ -1,40 +1,19 @@
 package org.acme.emailservice.email;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
-import org.acme.emailservice.email.oauth2.OAuth2Authenticator;
 import org.jboss.logging.Logger;
 
 import jakarta.mail.Folder;
-import jakarta.mail.MessagingException;
-// import jakarta.mail.Session;
-import jakarta.mail.Store;
 
 public class EmailService {
 
     private static Logger LOGGER = Logger.getLogger(EmailService.class);
 
-    // private String emailAddress;
-    // private String accessToken;
-    // private Store store;
     public Folder inbox;
-
-    // static ScheduledExecutorService monitor;
-    // static SynchronizeTask synchronizeTask;
 
     int nthreads = 2;
     Thread[] threads;
 
     public EmailService() {
-        // USER_NAME = getProperty("email.username");
-        // created = true;
-        // emails = new HashSet<Mail>();
-
-        // monitor = Executors.newScheduledThreadPool(0);
-        // synchronizeTask = new SynchronizeTask();
-        // OAuth2Authenticator.initialize();
 
         LOGGER.info("EmailService created");
 
@@ -45,28 +24,8 @@ public class EmailService {
 		}
     }
 
-    // public void login(String emailAddress, String accessToken) throws MessagingException {
-
-    //     if (emailAddress == null || accessToken == null) {
-    //         throw new IllegalArgumentException("Email address and access token fields are required");
-    //     }
-
-    //     this.emailAddress = emailAddress;
-    //     this.accessToken = accessToken;
-
-    //     OAuth2Authenticator.initialize();
-    //     this.store = OAuth2Authenticator.connectToImap(this.emailAddress, this.accessToken);
-
-    //     this.inbox = store.getFolder("INBOX");
-    //     // this.inbox.open(Folder.READ_ONLY);
-    // }
-
     public void shutdown() throws InterruptedException {
         LOGGER.info("EmailService shutdown");
-        // if (synchronizeTask != null)
-        //     synchronizeTask.stop();
-        // if (monitor != null)
-        //     monitor.shutdownNow();
         GmailClient.die = true;
         // wait for all the threads to finish
         // for (int i = 0; i < nthreads; i++) {
@@ -76,50 +35,4 @@ public class EmailService {
             threads[i].join();
         }
     }
-
-    // class SynchronizeTask implements Runnable {
-
-    //     boolean stop;
-    //     int load;
-    //     boolean settingCode;
-
-    //     public SynchronizeTask() {
-    //         LOGGER.info("EmailService/SynchronizeTask");
-    //         monitor.scheduleWithFixedDelay(this, 20, 20, TimeUnit.SECONDS);
-    //         stop = false;
-    //         load = 0;
-    //         settingCode = false;
-    //     }
-
-    //     public synchronized void run() {
-    //         try {
-    //             if (stop) {
-    //                 while (!Thread.currentThread().isInterrupted()) {
-    //                     Thread.currentThread().interrupt();
-    //                 }
-    //             } else {
-    //                 if (!this.equals(synchronizeTask)) {
-    //                     stop();
-    //                     return;
-    //                 }
-    //                 if (load != 0 && !settingCode) {
-    //                     LOGGER.info("EmailService...");
-    //                 } else
-    //                     load++;
-    //             }
-    //         } catch (Exception e) {
-    //             while (!Thread.interrupted()) {
-    //                 stop = true;
-    //                 Thread.currentThread().interrupt();
-    //             }
-    //         }
-    //     }
-
-    //     public void stop() {
-    //         while (!Thread.interrupted()) {
-    //             stop = true;
-    //             Thread.currentThread().interrupt();
-    //         }
-    //     }
-    // }
 }
--- src/main/java/org/acme/emailservice/email/GmailClient.java
+++ src/main/java/org/acme/emailservice/email/GmailClient.java
@@ -1,21 +1,105 @@
 package org.acme.emailservice.email;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.jboss.logging.Logger;
+
 public class GmailClient implements Runnable {
 
+    private static Logger LOGGER = Logger.getLogger(GmailClient.class);
+
+    private ScheduledExecutorService scheduler;
+    private SynchronizedTask runningTask;
+
     public static boolean die = false;
+
+    public GmailClient() {
+        // USER_NAME = getProperty("email.username");
+        // created = true;
+        // emails = new HashSet<Mail>();
+
+        scheduler = Executors.newScheduledThreadPool(0);
+        runningTask = new SynchronizedTask();
+        // OAuth2Authenticator.initialize();
+
+        LOGGER.info("GmailClient created");
+    }
 
     public void run() {
 
         while (!die) {
             try {
-                Thread.sleep(0);
+                Thread.sleep(10);
             } catch (InterruptedException e) {
-                System.out.println("Interrupted");
+                LOGGER.info("Interrupted");
                 return;
             }
             continue;
         }
-        System.out.println("GmailClient thread exited gracefully");
+        try {
+            this.shutdown();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        LOGGER.info("GmailClient thread exited gracefully");
     }
-    
+
+    public void shutdown() throws InterruptedException {
+        LOGGER.info("GmailClient shutdown");
+        if (runningTask != null)
+        runningTask.stop();
+        if (scheduler != null)
+        scheduler.shutdownNow();
+    }
+
+    class SynchronizedTask implements Runnable {
+
+        boolean stop;
+        int load;
+        boolean settingCode;
+
+        public SynchronizedTask() {
+            LOGGER.info("GmailClient/SynchronizedTask create");
+            scheduler.scheduleWithFixedDelay(this, 20, 20, TimeUnit.SECONDS);
+            stop = false;
+            load = 0;
+            settingCode = false;
+        }
+
+        public synchronized void run() {
+            try {
+                if (stop) {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        Thread.currentThread().interrupt();
+                    }
+                } else {
+                    if (!this.equals(runningTask)) {
+                        stop();
+                        return;
+                    }
+                    if (load != 0 && !settingCode) {
+                        LOGGER.info("GmailClient/SynchronizedTask run");
+                    } else
+                        load++;
+                }
+            } catch (Exception e) {
+                while (!Thread.interrupted()) {
+                    stop = true;
+                    Thread.currentThread().interrupt();
+                    LOGGER.info("GmailClient/SynchronizedTask Exception interupted");
+                }
+            }
+        }
+
+        public void stop() {
+            while (!Thread.interrupted()) {
+                stop = true;
+                Thread.currentThread().interrupt();
+                LOGGER.info("GmailClient/SynchronizedTask interrupted");
+            }
+        }
+    }
 }
